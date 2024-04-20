6499933cfdf836a2f1a89793f897a4ad972033a2
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-04-20 21:26:10 +0300

    add generics

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,8 +1,3 @@
-/* Створіть змінну, яка може містити або рядок, або число (union type)? Також, оголосіть змінну, яка може містити 
-лише одне з двох можливих рядкових значень: 'enable' або 'disable'(literal type)*/
-
 let input: string | number;
 
-type Button = "enable" | "disable";
-
-let primeButton: Button;
+let primeButton: "enable" | "disable";
---
+++ src/generics/1.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+interface Response {
+  name: string;
+  age: number;
+}
+
+async function fetchData(url: string): Promise<Response> {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
+// Типізуйте асинхронну функцію fetchData, яка приймає URL ресурсу та повертає об'єкт з даними. Використовуйте Generics для
+// типізації повернутих даних.
